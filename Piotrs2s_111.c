ebf86bc17b8b1bf53b5edb29632703c0af871427
Piotrs2s <Piotrs2s@wp.pl>
2017-10-21 23:23:39 +0200

    second commit

--- Piotrs2s_111.c
+++ Piotrs2s_111.c
@@ -8,10 +8,14 @@
 int main(){
     
 
- int n, x, y, i;
+ int n, x, y, i, r, temp1, temp2;
  srand(time(NULL));
- int r;
- int tab[10]
+ int tempY = 0;
+ int tempX = 0;
+ int tab1[1000];
+ int tab2[1000];
+ 
+
 
  printf("n = ");
  scanf("%d",&n);
@@ -24,19 +28,34 @@ int main(){
 
  if (1<=n && n<=pow(10,5) && 1<=x && x<=pow(10,12) && 1<=y && y<=pow(10,6))
  {
-
-  for(i = 0; i < n; )
+   for (i=0; i<n;)
 	{
-         r = rand();
-         if ();
- 	} 
+	  r = rand();
+
+          temp1 = tempX + pow(r,2);
+	  temp2 = tempY + r;
+	  
+          if (tempY <= y && tempX >=x )
+		{
+		  tab2[i] = r;
+		  tempX = tempX + pow(tab2[i],2);
+		  tempY = tempY + tab2[i];
+                  printf("%d\n",tab2[i]);
+                  i++;
+		
+		}
+	   else
+		{
+		 n++;
+		 i++;		
+		} 
+	}
  }
  else
  {
- printf("Output: -1\n");
+ printf("-1\n");
  }
  
-  //printf("input:\n n= %d\n x= %d\n y= %d\n",n, x, y);
 
 
 
