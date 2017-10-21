f77824769ce0471050f7a45759decd7ac05c27b6
Piotrs2s <Piotrs2s@wp.pl>
2017-10-21 21:56:19 +0200

    Program 111 first commit

---
+++ Piotrs2s_111.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+
+int main(){
+    
+
+ int n, x, y, i;
+ srand(time(NULL));
+ int r;
+ int tab[10]
+
+ printf("n = ");
+ scanf("%d",&n);
+
+ printf("x = ");
+ scanf("%d",&x);
+
+ printf("y = ");
+ scanf("%d",&y);
+
+ if (1<=n && n<=pow(10,5) && 1<=x && x<=pow(10,12) && 1<=y && y<=pow(10,6))
+ {
+
+  for(i = 0; i < n; )
+	{
+         r = rand();
+         if ();
+ 	} 
+ }
+ else
+ {
+ printf("Output: -1\n");
+ }
+ 
+  //printf("input:\n n= %d\n x= %d\n y= %d\n",n, x, y);
+
+
+
+
+return 0;    
+}
